be5abc3aef1795a057f0695f485d7167a0590824
fabioporzio <fabio.porzio00@gmail.com>
2025-12-09 13:11:24 +0100

    User's password checks are now enabled

--- src/main/java/com/trello/clone/web/model/user/CreateUserRequest.java
+++ src/main/java/com/trello/clone/web/model/user/CreateUserRequest.java
@@ -2,6 +2,7 @@ package com.trello.clone.web.model.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class CreateUserRequest {
@@ -14,12 +15,11 @@ public class CreateUserRequest {
     private String username;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
-    /*
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Pattern(
             regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-            message = "Password must contain uppercase, lowercase, number, and special character")
-    */
+            message = "Password must contain uppercase, lowercase, number, and special character"
+    )
     private String password;
 
     public String getEmail() {
