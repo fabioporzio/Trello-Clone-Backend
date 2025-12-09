f31e7565f93082cfc646c687557a4ce48592f09b
fabioporzio <fabio.porzio00@gmail.com>
2025-12-09 13:01:42 +0100

    In NotificationService added check for addNotifications methods

--- src/main/java/com/trello/clone/service/NotificationService.java
+++ src/main/java/com/trello/clone/service/NotificationService.java
@@ -1,10 +1,16 @@
 package com.trello.clone.service;
 
 import com.trello.clone.data.model.Notification;
+import com.trello.clone.data.model.Project;
+import com.trello.clone.data.model.Task;
 import com.trello.clone.data.repository.NotificationRepository;
+import com.trello.clone.data.repository.ProjectRepository;
+import com.trello.clone.data.repository.TaskRepository;
+import com.trello.clone.service.exception.NotFoundException;
 import com.trello.clone.web.model.notification.CreateNotificationRequest;
 import com.trello.clone.web.model.notification.NotificationResponse;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,9 +21,17 @@ import java.util.Map;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
 
-    public NotificationService(NotificationRepository notificationRepository) {
+    public NotificationService(
+            NotificationRepository notificationRepository,
+            TaskRepository taskRepository,
+            ProjectRepository projectRepository
+    ) {
         this.notificationRepository = notificationRepository;
+        this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
     }
 
     public Map<String, List<NotificationResponse>> getReceivedNotifications(String email) {
@@ -37,17 +51,26 @@ public class NotificationService {
 
     public void addProjectNotification(
             CreateNotificationRequest createNotificationRequest,
-            Object projectId,
+            ObjectId projectId,
             String senderEmail
     ) {
+        Project project = projectRepository.findById(projectId);
+        if (project == null) {
+            throw new NotFoundException("Project with ID " + projectId + " not found");
+        }
+
         this.notificationRepository.addProjectNotification(createNotificationRequest, projectId, senderEmail);
     }
 
     public void addTaskNotification(
             CreateNotificationRequest createNotificationRequest,
-            Object taskId,
+            ObjectId taskId,
             String senderEmail
     ) {
+        Task task = taskRepository.findById(taskId);
+        if (task == null) {
+            throw new NotFoundException("Task with ID " + taskId + " not found");
+        }
         this.notificationRepository.addTaskNotification(createNotificationRequest, taskId, senderEmail);
     }
 
